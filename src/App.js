a2a6e44042e1d09f5119a1790421f623ca39a40a
SilverMaiden <h.specials@gmail.com>
2020-01-27 14:50:14 -0800

    Added Norse font

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from "./components/LandingPage";
 import Game from "./components/Game/Game";
 import Highscores from "./components/Highscores";
-
 import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import {GameContext} from "./contexts/GameContext";
+import axios from 'axios';
+
 
 function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://www.random.org/integers/?num=4&min=1&max=6&col=1&base=10&format=plain&rnd=new")
+        .then(response => {
+            setData(response.data)
+        })
+    }, [])
+
   return (
-    <Router>
-        <div className="App">
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/game" component={Game} />
-        <Route exact path="/highscores" component={Highscores} />
-        </div>
-    </Router>
+    <GameContext.Provider value={data}>
+        <Router>
+            <div className="App">
+            {console.log(data)}
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/game" component={Game} />
+            <Route exact path="/highscores" component={Highscores} />
+            </div>
+        </Router>
+    </GameContext.Provider>
   );
 }
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,14 +1,26 @@
-import React from "react";
-
+import React, {useState, useContext} from "react";
+import {GameContext} from "../../contexts/GameContext";
 import NumberSlider from "./NumberSlider";
 import Timer from "./Timer";
 import Attempts from "./Attempts";
 
 const Game = () => {
+    const randomNums = useContext(GameContext);
+    const norseChars = {
+        0: 'a',
+        1: 'b',
+        2: "c",
+        3: "d",
+        4: "e",
+        5: "f",
+        6: "g",
+        7: "h"
+    }
+    console.log(typeof randomNums)
 
     return (
         <div>
-            <h1> This is the game page.</h1>
+            <h1 className="norse" > This is the game page.</h1>
             <NumberSlider />
             <Timer />
             <Attempts />
