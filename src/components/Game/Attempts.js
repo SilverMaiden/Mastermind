add2aa91d6393297da3cd9520044c6d309c39336
SilverMaiden <h.specials@gmail.com>
2020-01-28 12:50:57 -0800

    Have basic game functionality working, now need to separate it out in  easy vs hard mode

--- src/components/Game/Attempts.js
+++ src/components/Game/Attempts.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const Attempts = () => {
+const Attempts = (props) => {
+    let message;
 
     return (
-        <h1> This is the Attempts component.</h1>
+        <div>
+        {props.data.firstAttempt ? <h3> Enter the correct combination</h3> : <h3>Pattern Not Matched.</h3>}
+        <h6> Attempts Remaining: {props.data.attempts} </h6>
+        { props.data.rightNums > 0 && props.data.rightPositions === props.data.rightNums ?
+            <p> However, you have guessed {props.data.rightNums} of the runes correctly, and {props.data.rightPositions} of the positions.</p>
+        : <p> You have guessed {props.data.rightNums} of the runes correctly.  </p>}
+
+        </div>
     )
 }
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,45 +3,129 @@ import {GameContext} from "../../contexts/GameContext";
 import NumberSlider from "./NumberSlider";
 import Timer from "./Timer";
 import Attempts from "./Attempts";
+import Lose from "../Lose";
+import Win from "../Win";
 
 const Game = (props) => {
     const {data, addChosen} = useContext(GameContext);
+    const [gameState, setGameState] = useState({
+        rightNums: 0,
+        rightPositions: 0,
+        attempts: 10,
+        pass: false,
+        firstAttempt: true,
+    });
 
-    /*
-    let randomNumsArr = data.randomNums.split("").filter(element => (
-        element in ["0", "1", "2", "3", "4", "5", "6", "7"]
-    ))
-    */
-    const norseChars = {
-        0: 'a',
-        1: 'b',
-        2: "c",
-        3: "d",
-        4: "e",
-        5: "f",
-        6: "g",
-        7: "h"
+    const [rightAnswers, setRightAnswers]= useState({
+        first: false,
+        second: false,
+        third: false,
+        fourth: false
+    })
+    console.log(gameState)
+
+
+    const handleSubmit = () => {
+        //Compare data.randomNums with data.chosenNums
+        //Reset rightNums and rightPositions to 0
+        //
+        let firstAttemptSwitch;
+        setGameState({...gameState, rightNums: 0, rightPositions: 0})
+        if (gameState.firstAttempt === true) {
+            firstAttemptSwitch = false;
+        } else {
+            firstAttemptSwitch = false;
+        }
+
+        //Initialize variables;
+        let correctPositions = 0;
+        let correctNums = 0;
+        let valid = true;
+        let validFirst = false;
+        let validSecond = false
+        let validThird = false;
+        let validFourth = false;
+
+        //Creating an array of answers the use has chosen
+        //to be able to use array methods
+        let chosenAnswersArr = [data.chosenNums.first,
+            data.chosenNums.second,
+            data.chosenNums.third,
+            data.chosenNums.fourth
+        ]
+
+        //On submit, attempts will decrease by 1
+        let updatedAttempts = gameState.attempts - 1;
+
+        //Checking each position for correct answer
+        if (data.randomNums[0] === data.chosenNums.first) {
+            correctPositions += 1;
+            validFirst = true;
+        }
+        if (data.randomNums[1] === data.chosenNums.second) {
+            correctPositions += 1;
+            validSecond = true;
+        }
+
+        if (data.randomNums[2] === data.chosenNums.third) {
+            correctPositions += 1;
+            validThird = true;
+        }
+
+        if (data.randomNums[3] === data.chosenNums.fourth) {
+            correctPositions += 1;
+            validFourth = true;
+        }
+
+        //Checking chosen answer for correctly chosen numbers (but not positions)
+        chosenAnswersArr.forEach(num => {
+            if (data.randomNums.includes(num)) {
+                correctNums += 1;
+            }
+        })
+
+        setRightAnswers({...rightAnswers,
+                        first: validFirst,
+                        second: validSecond,
+                        third: validThird,
+                        fourth: validFourth
+        })
+
+        setGameState({...gameState,
+                     attempts: updatedAttempts,
+                     rightNums: correctNums,
+                     rightPositions: correctPositions,
+                     firstAttempt: firstAttemptSwitch,
+        });
+
+        if (correctPositions === 4) {
+            setGameState({...gameState, pass: true})
+        } else {
+            console.log("Sorry, you got it wrong.");
+            console.log(`Attempts remaining: ${gameState.attempts}`)
+        }
     }
-    let location = 0;
-
-  const [position, setPosition] = useState({
-      first: 0,
-      second: 0,
-      third: 0,
-      fourth: 0
-  })
 
     return (
-        <div className="container">
-            <h1> This is the game page.</h1>
+        <div>
+        {gameState.pass ? <Win /> :
+            <div>
+            {gameState.attempts !== 0 ?
+                <div className="container">
+                    <h1> This is the game page.</h1>
+                    <Attempts data={gameState}/>
 
-            <div className="inputContainer">
-                <NumberSlider data={data} addChosen={addChosen} norseChars={norseChars} location={"first"} />
-                <NumberSlider data={data} addChosen={addChosen} location={"second"} />
-                <NumberSlider data={data} addChosen={addChosen} location={"third"} />
-                <NumberSlider data={data} addChosen={addChosen} location={"fourth"} />
-
+                    <div className="inputContainer">
+                        <NumberSlider data={data} addChosen={addChosen} location={"first"} />
+                        <NumberSlider data={data} addChosen={addChosen} location={"second"} />
+                        <NumberSlider data={data} addChosen={addChosen} location={"third"} />
+                        <NumberSlider data={data} addChosen={addChosen} location={"fourth"} />
+                    </div>
+                    <button onClick={handleSubmit}> Submit </button>
+                </div> : <Lose />
+            }
             </div>
+        }
         </div>
     )
 }
