0f414bf27f9d82e66508655e3add462e7c5cda01
SilverMaiden <h.specials@gmail.com>
2020-01-28 13:03:04 -0800

    Create routing to game for easy and hard mode from landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useContext} from "react";
+import {Link} from "react-router-dom";
+import {GameContext} from "../contexts/GameContext";
 import Game from "./Game/Game";
 
 const LandingPage = () => {
+    const {selectMode} = useContext(GameContext);
+
+    const handleClick = (mode) => {
+        selectMode(mode);
+    }
 
     return (
         <div>
             <h1> This is the landing page.</h1>
-            <Game />
+            <h4> Select difficulty:</h4>
+           <Link to="/game"><button onClick={() => handleClick("easyMode")}> Easy Mode </button></Link>
+           <Link to="/game"><button onClick={() => handleClick("hardMode")}> Hard Mode </button></Link>
         </div>
     )
 }
